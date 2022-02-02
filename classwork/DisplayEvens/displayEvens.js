53c900fe34cdddf466ef2131262c9e81a6845cc5
Sweetlana Protsenko <svitprotsenko@gmail.com>
2022-02-01 23:16:44 -0700

    Carousel

--- classwork/DisplayEvens/displayEvens.js
+++ classwork/DisplayEvens/displayEvens.js
@@ -16,13 +16,13 @@ function validateItems() {
     var step = document.forms["evens"]["step"].value;
     if (startingNum == "" || isNaN(startingNum)) {
         alert("Starting number must be filled in with a namber.");
-     document.forms["evens"]["startingNum"].parentElement.className = "form-group has-error"
+        document.forms["evens"]["startingNum"].parentElement.className = "form-group has-error"
         document.forms["evens"]["startingNum"].focus();
         return false;
     }
     if (endingNum == "" || isNaN(endingNum)) {
-        alert("Ending number must be filled in with a namber.");     
-     document.forms["evens"]["endingNum"].parentElement.className = "form-group has-error"
+        alert("Ending number must be filled in with a namber.");
+        document.forms["evens"]["endingNum"].parentElement.className = "form-group has-error"
         document.forms["evens"]["endingNum"].focus();
         return false;
     }
@@ -42,9 +42,47 @@ function validateItems() {
         alert("Ending Number must be greater than Starting Number.");
         document.forms["evens"]["step"].parentElement.className = "form-group has-error"
         document.forms["evens"]["step"].focus();
+
+         
         return false;
     }
-    document.getElementById
-    
 }
+    
+document.getElementById("evensList").innerText = getEvensList(startingNum, endingNum, step);
+document.getElementById("result").style.display = "block";
+document.getElementById("startingNum").innerText = startingNum;
+document.getElementById("endingNum").innerText = endingNum;
+document.getElementById("step").innerText = step;
+document.getElementById("evensList").innerHTML = getEvensList(startingNum, endingNum, step)
+            .join("<br />");
+
+function getEvensList(startingNum, endingNum, step) {
+    var num1 = parseInt(startingNum.value, 3)
+    var num2 = parseInt(endingNum.value, 3);
+    var step = parseInt(step);
+    var evensList = new Array[i];
+    for (var i = num1; i <= num2; i = i + step) {
+        if (i % 2 == 0) {
+            evensList.push(i);
+        }
+    }
+    return evensList;
+}
+
+    /*console.log(document.getElementById("result"));
+                document.write("<table>")
+    var startingNum = prompt("Starting Number");
+    var endingNum = prompt("end num");
+    var step = prompt("step");
+    
+        for (var i = startingNum; i <= endingNum; i=i+step) {
+            if (i % 2 == 0) {
+                document.write("<td>"+i+"</td></tr>");
+            } else {
+               document.write("<td>"+i+"</td></tr>"); 
+            }
+        }
+    document.write("</table>");
+    
+
 
