066c899af6efabe707250a02dfe2f35a3b3af7da
Sweetlana Protsenko <svitprotsenko@gmail.com>
2022-02-04 19:50:15 -0700

    call out a big news

--- restaurantSite/validate.js
+++ restaurantSite/validate.js
@@ -1,7 +1,20 @@
 function validate() {
+
     contactForm.className = "needs-validation";
     if (!contactForm.checkValidity()) {
         contactForm.className = "was-validated";
         return false;
     }
+    var phoneNumber = document.forms["contactForm"]["phoneNumber"].value;
+    if (phoneNumber == "" || isNaN(phoneNumber)) {
+        alert("Phone Number must be filled in with a numbers");
+        return false
+    }
+    let x = document.forms["contactForm"]["name"].value;
+    if (x = "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    return false;
 }
